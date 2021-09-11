e4c932af7e9d86562e1899535cab78d78c326643
ZimaBlu <benskipp@gmail.com>
2021-09-11 10:47:40 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def capital_indexes(word):
-    capital_list = []
-    for i in range(0, len(word)):
-        if word[i].isupper():
-            capital_list.append(i)
-    print(capital_list)
-
-capital_indexes("HeLlO")
